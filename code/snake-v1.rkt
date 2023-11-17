fe1ec577c4b10bdaa0f9465a19ca066a41b52668
Simone Maccario <macas@usi.ch>
2023-11-17 14:14:27 +0100

    update the code in racket

--- code/snake-v1.rkt
+++ code/snake-v1.rkt
@@ -2,6 +2,7 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname snake-v1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; CODE STANDARDS ;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ; - Constants always UPPERCASE
 ; - Functions always Capitalize
 
@@ -21,9 +22,11 @@
 
 ; a SnakeUnit is an Image
 ; It represents the little rectangles that the snake is made off
-(define SnakeUnit (rectangle 25 25 "solid" "green"))
+(define SnakeUnit (rectangle 24 24 "solid" "green"))
 
-(define AppleUnit (rectangle 25 25 "solid" "red"))
+; a AppleUnit is an Image
+; It represents the little rectangles that the apple is made off
+(define AppleUnit (rectangle 24 24 "solid" "red"))
 
 
 
@@ -34,10 +37,14 @@
 
 ;;;;;;;;;; POSITION ;;;;;;;;;;
 
-; a Position is a Posn as such:
+; a Position is Posn as such:
 ; - (make-posn -1 -1)
-; - (make-posn (- (posn-x Position) 26) (- (posn-y Position) 26))
+; - (make-posn (- (posn-x Position) 25) (- (posn-y Position) 25))
 ; represents the position of a element of the snake
+
+; a List<Positions> is a one of:
+; - (cons Position '())
+; - (cons Position (cons List<Position> '()))
 
 
 
@@ -64,12 +71,12 @@
 ;    [else ... state ...]))                                                                               ; recursive case
 
 ; Code
-(define (dimension state)
+(define (dimension Snake)
   (cond
-    [(and (= (snake-length state) 3) (equal? (make-posn -27 -27) (snake-position state))) state] ; snake direction, length and direction are the same
-    [else (make-snake (snake-position state)                                                     ; snake position is the same
-          (add1 (snake-length state))                                                            ; snake length become one more bigger
-          (snake-direction state))]))                                                            ; snake direction is the same
+    [(and (= (snake-length Snake) 3) (equal? (make-posn -26 -26) (snake-position Snake))) Snake] ; snake direction, length and direction are the same
+    [else (make-snake (snake-position Snake)                                                     ; snake position is the same
+          (add1 (snake-length Snake))                                                            ; snake length become one more bigger
+          (snake-direction Snake))]))                                                            ; snake direction is the same
 
 
 
@@ -77,13 +84,13 @@
 
 ; a Direction is one of these String
 ; - "up"
-; - "down"
 ; - "left"
+; - "down"
 ; - "right"
 
 (define Up "up")
-(define Down "down")
 (define Left "left")
+(define Down "down")
 (define Right "right")
 
 
@@ -99,10 +106,11 @@
 
 (define-struct snake [position length direction])
 
-(define Snake1 (make-snake (make-posn -27 -27) 3 Up))
-(define Snake2 (make-snake (make-posn -27 -27) 4 Down))
-(define Snake3 (make-snake (make-posn -27 -53) 5 Left))
-(define Snake4 (make-snake (make-posn -53 -27) 6 Right))
+(define SnakeDefault (make-snake (make-posn -26 -26) 3 Right))
+(define Snake1 (make-snake (make-posn -26 -26) 3 Up))
+(define Snake2 (make-snake (make-posn -26 -26) 4 Down))
+(define Snake3 (make-snake (make-posn -26 -52) 5 Left))
+(define Snake4 (make-snake (make-posn -52 -26) 6 Right))
 (define Snake5 (make-snake (make-posn -1 -1) 3 Left))
 
 
@@ -112,19 +120,21 @@
 ; an Apple is a Position
 ; it represents the position of the apple at a particular moment
 
-(define Apple1 (make-posn -27 -27))
-(define Apple2 (make-posn -1 -27))
+(define Apple1 (make-posn -26 -26))
+(define Apple2 (make-posn -1 -26))
 
 
 
 ;;;;;;;;;; GAME ;;;;;;;;;;
 
-; a Maybe<Game> is one of:
+; a Game is one of:
 ; - #true : the game is running
-; - #false : the game is on pause
+; - #false : the game is lost and the application is on pause
 
-(define Maybe<Game>_t #true)
-(define Maybe<Game>_f #false)
+(define Game_t #true)
+(define Game_f #false)
+
+
 
 ;;;;;;;;;; QUIT ;;;;;;;;;;
 
@@ -132,62 +142,235 @@
 ; - #false : the game is on and is running
 ; - #true : the game is off and "close"
 
+(define quit_f #false)
+(define quit_t #true)
 
 
-;;;;;;;;;; APPSTATE ;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;; APPSTATE ;;;;;;;;;;;;;;;;;;;;
 
 ; an AppState is a Struct
 ; (make-appstate snake apple game quit)
 ; where:
 ; - snake is a Snake
 ; - apple is an Apple
-; - game is a Maybe<Game>
-; - quit is a Boolean
+; - game is a Game
+; - quit is a Quit
 
 (define-struct appstate [snake apple game quit])
 
+(define EDefault (make-appstate SnakeDefault Apple1 Game_t quit_f))
+(define E1 (make-appstate Snake1 Apple1 Game_t quit_f))
+(define E2 (make-appstate Snake2 Apple1 Game_t quit_f))
+(define E3 (make-appstate Snake3 Apple1 Game_t quit_f))
+(define E4 (make-appstate Snake4 Apple1 Game_t quit_f))
+(define E5 (make-appstate Snake5 Apple1 Game_t quit_f))
+(define E6 (make-appstate Snake1 Apple1 Game_f quit_f))
+(define E7 (make-appstate Snake1 Apple1 Game_t quit_t))
 
 
-; change-snake-direction : Direction Snake -> Snake
-; changes the direction of the snake
+
+
+
+;;;;;;;;;;;;;;;;;;;; DRAW ;;;;;;;;;;;;;;;;;;;;
+
+; draw : AppState -> AppState
+; draw the snake and the apple's position on the background
+
+
+
+;;;;;;;;;;;;;;;;;;;; TICK ;;;;;;;;;;;;;;;;;;;;
+
+; tick : AppState Number -> Number
+; change the position of the snake and apple's position, if it was eaten by snake, and the tick is become faster and faster every time the snake eats an apple
+
+
+
+
+;;;;;;;;;;;;;;;;;;;; HANDLE KEYBOARD ;;;;;;;;;;;;;;;;;;;;
+
+; an handle-keyboard is one of:
+; - up    : call the fuction to change the snake's direction to up
+; - right : call the fuction to change the snake's direction to right
+; - down  : call the fuction to change the snake's direction to down
+; - left  : call the fuction to change the snake's direction to left
+; - r     : call the fuction to reset the game
+; - esc   : call the fuction to close main application 
+; - Any   : return the prevoius AppState
 
 ; Example
-(check-expect (change-snake-direction "down" Snake1) (make-snake (make-posn -27 -27) 3 "down"))
-(check-expect (change-snake-direction "up" Snake2) (make-snake (make-posn -27 -27) 4 "up"))
-(check-expect (change-snake-direction "left" Snake3) (make-snake (make-posn -27 -53) 5 "left"))
-(check-expect (change-snake-direction "right" Snake4) (make-snake (make-posn -53 -27) 6 "right"))
-(check-expect (change-snake-direction "up" Snake5) (make-snake (make-posn -1 -1) 3 "up"))
+(check-expect (handle-keyboard "up" E2) (make-appstate (make-snake (make-posn -26 -26) 4 Up) Apple1 Game_t quit_f))
+(check-expect (handle-keyboard "right" E1) (make-appstate (make-snake (make-posn -26 -26) 3 Right) Apple1 Game_t quit_f))
+(check-expect (handle-keyboard "down" E1) (make-appstate (make-snake (make-posn -26 -26) 3 Down) Apple1 Game_t quit_f))
+(check-expect (handle-keyboard "left" E1) (make-appstate (make-snake (make-posn -26 -26) 3 Left) Apple1 Game_t quit_f))
+(check-expect (handle-keyboard "r" E1) EDefault)
+(check-expect (handle-keyboard "/escape" E1) #false)
+(check-expect (handle-keyboard "/escape" E7) #true)
+(check-expect (handle-keyboard "" E1) E1)
+(check-expect (handle-keyboard " " E1) E1)
+(check-expect (handle-keyboard "ciao" E1) E1)
+(check-expect (handle-keyboard "b" E1) E1)
+(check-expect (handle-keyboard #false E1) E1)
+(check-expect (handle-keyboard #true E1) E1)
+(check-expect (handle-keyboard 2 E1) E1)
 
 ; Template
-;(define (change-snake-direction dir snake)
+;(define (handle-mouse key state)
 ;  (cond
-;    [(string=? dir "down") ... state ...]
-;    [(string=? dir "right") ... state ...]
-;    [(string=? dir "left") ... state ...]
-;    [(string=? dir "up") ... state ...]))
+;    [(not (string? key)) ... state ...]
+;    [(string=? key "up") ... state ...]
+;    [(string=? key "right") ... state ...]
+;    [(string=? key "down") ... state ...]
+;    [(string=? key "left") ... state ...]
+;    [(string=? key "r") ... state ...]
+;    [(string=? key "/escape") ... state ...]
+;    [else ... state ...]))
 
 ; Code
-(define (change-snake-direction dir snake)
+(define (handle-keyboard key state)
   (cond
-    [(string=? dir "down") (make-snake (snake-position snake) (snake-length snake) "down")]
-    [(string=? dir "right") (make-snake (snake-position snake) (snake-length snake) "right")]
-    [(string=? dir "left") (make-snake (snake-position snake) (snake-length snake) "left")]
-    [(string=? dir "up") (make-snake (snake-position snake) (snake-length snake) "up")]))
+    [(not (string? key)) state]                                  ; for any possible not-key input which are not a string, the output is the same AppState as before
+    [(string=? key "up") (change-snake-direction key state)]     ; change the direction to up
+    [(string=? key "right") (change-snake-direction key state)]  ; change the direction to right
+    [(string=? key "down") (change-snake-direction key state)]   ; change the direction to down
+    [(string=? key "left") (change-snake-direction key state)]   ; change the direction to left
+    [(string=? key "r") (reset state)]                           ; reset the game
+    [(string=? key "/escape") (end? state)]                      ; quit the game
+    [else state]))                                               ; for any possible not-key input which are a string, the output is the same AppState as before
+
+
+;;;;;;;;;; CHANGE SNAKE DIRECTION ;;;;;;;;;;
+; change-snake-direction : Direction AppState -> AppState
+; changes the direction of the snake
+; Header (define (change-snake-direction dir state) )
+
+; Example
+(check-expect (change-snake-direction "up" E2) (make-appstate (make-snake (make-posn -26 -26) 4 Up) Apple1 Game_t quit_f))
+(check-expect (change-snake-direction "right" E1) (make-appstate (make-snake (make-posn -26 -26) 3 Right) Apple1 Game_t quit_f))
+(check-expect (change-snake-direction "down" E1) (make-appstate (make-snake (make-posn -26 -26) 3 Down) Apple1 Game_t quit_f))
+(check-expect (change-snake-direction "left" E1) (make-appstate (make-snake (make-posn -26 -26) 3 Left) Apple1 Game_t quit_f))
+
+
+; Template
+;(define (change-snake-direction dir state)
+;  (cond
+;    [(string=? dir "up") ... state ...]
+;    [(string=? dir "left") ... state ...]
+;    [(string=? dir "down") ... state ...]
+;    [(string=? dir "right") ... state ...]))
+
+; Code
+(define (change-snake-direction dir state)
+  (cond
+    [(string=? dir "up") (make-appstate (update-snake-position dir (appstate-snake state)) (appstate-apple state) (appstate-game state) (appstate-quit state))]     ; change the direction to up
+    [(string=? dir "right") (make-appstate (update-snake-position dir (appstate-snake state)) (appstate-apple state) (appstate-game state) (appstate-quit state))]  ; change the direction to right
+    [(string=? dir "down") (make-appstate (update-snake-position dir (appstate-snake state)) (appstate-apple state) (appstate-game state) (appstate-quit state))]   ; change the direction to down
+    [(string=? dir "left") (make-appstate (update-snake-position dir (appstate-snake state)) (appstate-apple state) (appstate-game state) (appstate-quit state))])) ; change the direction to left
+
+
+
+;;;;;;;;;; UPDATE SNAKE DIRECTION ;;;;;;;;;;
 
 ; update-snake-position : Direction Snake -> Snake
 ; updates snake position based on snake direction
 
-(define (update-snake-direction dir snake)
-  (cond
-    [(string=? dir "down") (make-snake (snake-position snake) (snake-length snake) "down")]
-    [(string=? dir "right") (make-snake (snake-position snake) (snake-length snake) "right")]
-    [(string=? dir "left") (make-snake (snake-position snake) (snake-length snake) "left")]
-    [(string=? dir "up") (make-snake (snake-position snake) (snake-length snake) "up")]))
+; Examples
+(check-expect (update-snake-position "up" Snake2) (make-snake (make-posn -26 -26) 4 "up"))
+(check-expect (update-snake-position "right" Snake4) (make-snake (make-posn -52 -26) 6 "right"))
+(check-expect (update-snake-position "down" Snake1) (make-snake (make-posn -26 -26) 3 "down"))
+(check-expect (update-snake-position "left" Snake3) (make-snake (make-posn -26 -52) 5 "left"))
+(check-expect (update-snake-position "up" Snake5) (make-snake (make-posn -1 -1) 3 "up"))
 
+; Template
+;(define (update-snake-position dir snake)
+;  (cond
+;    [(string=? dir "up") ... snake ...]
+;    [(string=? dir "left") ... snake ...]
+;    [(string=? dir "right") ... snake ...]
+;    [(string=? dir "down") ... snake ...]
+
+; Code
+(define (update-snake-position dir snake)
+  (cond
+    [(string=? dir "up") (make-snake (snake-position snake) (snake-length snake) "up")]        ; change the direction to up
+    [(string=? dir "right") (make-snake (snake-position snake) (snake-length snake) "right")]  ; change the direction to right
+    [(string=? dir "down") (make-snake (snake-position snake) (snake-length snake) "down")]    ; change the direction to down
+    [(string=? dir "left") (make-snake (snake-position snake) (snake-length snake) "left")]))  ; change the direction to left
+
+
+;;;;;;;;;; RESET ;;;;;;;;;;
+
+; a reset is a Boolean
+; reset : AppState -> AppState
+; if the r button is pressed, the main application is reseted
+; Header (define (end? state) #false)
+
+; Examples
+;(check-expect (reset E1) E1)
+;(check-expect (reset E2) E2)
+;(check-expect (reset E3) E3)
+;(check-expect (reset E4) E4)
+;(check-expect (reset E5) E5)
+(check-expect (reset E1) EDefault)
+(check-expect (reset E2) EDefault)
+(check-expect (reset E3) EDefault)
+(check-expect (reset E4) EDefault)
+(check-expect (reset E5) EDefault)
+
+; Template
+;(define (reset state)
+;  (cond
+;    [(boolean=? (appstate-quit state) #true) ... state ...]
+;    [else ... state ...]))
+
+; Code
+(define (reset state)
+  (cond
+    [(boolean=? (appstate-quit state) #true) state] ; the game continue to run
+    [else EDefault]))                               ; the game is reseted
+
+
+
+;;;;;;;;;; QUIT ;;;;;;;;;;
+
+; a quit is a Boolean
+; end? : AppState -> Boolean
+; if the escape button is pressed, the main application is closed
+; Header (define (end? state) #false)
+
+; Examples
+(check-expect (end? E1) #false)
+(check-expect (end? E2) #false)
+(check-expect (end? E3) #false)
+(check-expect (end? E4) #false)
+(check-expect (end? E5) #false)
+(check-expect (end? (make-appstate Snake1 Apple1 Game_t quit_t)) #true)
+(check-expect (end? (make-appstate Snake2 Apple1 Game_t quit_t)) #true)
+(check-expect (end? (make-appstate Snake3 Apple1 Game_t quit_t)) #true)
+(check-expect (end? (make-appstate Snake4 Apple1 Game_t quit_t)) #true)
+(check-expect (end? (make-appstate Snake5 Apple1 Game_t quit_t)) #true)
+
+
+; Template
+;(define (end? state)
+;  (cond
+;    [(boolean=? (appstate-quit state) #false) ... state ...]
+;    [else ... state ...]))
+
+; Code
+(define (end? state)
+  (cond
+    [(boolean=? (appstate-quit state) #false) #false] ; the application remains on
+    [else #true]))                                    ; the application turn off
+
+
+
+
+;;;;;;;;;;;;;;;;;;;; MAIN APPLICATION ;;;;;;;;;;;;;;;;;;;;
 
 ;(define (snake-game AppState)
 ;  (big-bang AppState
 ;    [to-draw draw]             ; draw the snake
 ;    [on-tick move-snake time]  ; uptade snake's position and "time" incrase each tick
-;    [on-key handle-mouse]      ; change snake's direction or reset game or quit the game
+;    [on-key handle-keyboard]   ; change snake's direction or reset game or quit the game
 ;    [stop-when end?]))         ; quit the application
